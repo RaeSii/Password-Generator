ba92508aa301862e17c7c2ea09c7d3d5452cdb70
raesii <leisolo@gmail.com>
2022-01-13 17:45:52 -0800

    confirm

--- script.js
+++ script.js
@@ -16,5 +16,12 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
-  
+  var passwordLength = parseInt( prompt("Enter Password Length?"))
+  if(passwordLength <8||passwordLength>126 || isNaN(passwordLength)) {
+    return "Invalid Length, please enter a valid value"
+  }
+var acceptUpper = confirm("Do you like to have Upper case letters in your password?")
+var acceptLower = confirm("Do you like to have Lower case letters in your password?")
+var acceptNumbers = confirm("Do you like to have Numbers in your password?")
+var acceptSpecial = confirm("Do you like to have Special Characters in your password?")
 }
